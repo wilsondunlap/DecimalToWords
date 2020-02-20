f9a1b88625582c68c0fcca3b23cfe74be6426fb3
wdunlap <wdunlap@securustechnologies.com>
2020-02-20 12:26:38 -0500

    Initial commit

---
+++ DecimalsToWords/Converter.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalsToWords
+{
+    internal class Converter
+    {
+
+        static internal string DecimalToWords(decimal number)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int intComponent = (int)number;
+
+            decimal decComponent = Math.Abs(number - (decimal)intComponent);
+
+            AppendIntegerString(intComponent, sb);
+
+            AppendPoint(decComponent, sb);
+
+            AppendDecimalString(decComponent, sb);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        static void AppendIntegerString(int intValue, StringBuilder sb)
+        {
+            if(intValue == 100)
+            {
+                sb.Append("One hundred");
+                return;
+            }
+
+            if(intValue == 0)
+            {
+                sb.Append(UniqueNumbers[0]);
+                return;
+            }
+
+            if(intValue < 0)
+            {
+                sb.Append("negative ");
+                intValue *= -1;
+            }
+
+            while(intValue > 0)
+            {
+                if(intValue >= 20 || intValue / 10 <= 99)
+                {
+                    int tensValue = intValue / 10;
+                    if(tensValue >= 2)
+                    {
+                        sb.Append(dictTensValues[tensValue * 10]);
+                        intValue -= tensValue * 10;
+                    } else
+                    {
+                        sb.Append(UniqueNumbers[intValue]);
+                        intValue = 0;
+                    }
+                }
+            }
+        }
+        
+        static void AppendPoint(decimal decValue, StringBuilder sb)
+        {
+            if(decValue.ToString().IndexOf('.') != -1)
+            {
+                sb.Append("point ");
+            }
+        }
+
+        static void AppendDecimalString(decimal decValue, StringBuilder sb)
+        {
+            string strDec = decValue.ToString();
+
+            string[] arrIntegerAndDecimalParts = decValue.ToString().Split(new char[] { '.' });
+
+            if(arrIntegerAndDecimalParts.Length < 2)
+            {
+                return;
+            }
+
+            foreach(char numeral in arrIntegerAndDecimalParts[1])
+            {
+                sb.Append(singleDigits[numeral]);
+            }
+        }
+
+        static string[] UniqueNumbers = new string[]
+        {
+            "zero ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine ",
+            "ten ", "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ",
+            "eighteen ", "nineteen "
+        };
+
+        static Dictionary<char, string> singleDigits = new Dictionary<char, string>()
+        {
+            { '0', UniqueNumbers[0] },
+            { '1', UniqueNumbers[1] },
+            { '2', UniqueNumbers[2] },
+            { '3', UniqueNumbers[3] },
+            { '4', UniqueNumbers[4] },
+            { '5', UniqueNumbers[5] },
+            { '6', UniqueNumbers[6] },
+            { '7', UniqueNumbers[7] },
+            { '8', UniqueNumbers[8] },
+            { '9', UniqueNumbers[9] }
+        };
+
+        static Dictionary<int, string> dictTensValues = new Dictionary<int, string>()
+        {
+            { 20, "twenty " },
+            { 30, "thirty " },
+            { 40, "forty " },
+            { 50, "fifty " },
+            { 60, "sixty " },
+            { 70, "seventy " },
+            { 80, "eighty " },
+            { 90, "ninety " }
+        };
+    }
+}
---
+++ DecimalsToWords/Program.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalsToWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string negativeOnePointOne = Converter.DecimalToWords(-1.1M);
+
+            string negativeNineteenPointNineFiveZeroZero = Converter.DecimalToWords(-19.9500M);
+
+            string onePointOne = Converter.DecimalToWords(1.1M);
+            string twoPointTwo = Converter.DecimalToWords(2.2M);
+            string threePointThree = Converter.DecimalToWords(3.3M);
+            string fourPointFour = Converter.DecimalToWords(4.4M);
+            string fivePointFive = Converter.DecimalToWords(5.5M);
+            string sixPointSix = Converter.DecimalToWords(6.6M);
+            string sevenPointSeven = Converter.DecimalToWords(7.7M);
+            string eightPointEight = Converter.DecimalToWords(8.8M);
+
+            string ninePointNine = Converter.DecimalToWords(9.9M);
+            string tenPointZero = Converter.DecimalToWords(10.0M);
+            string elevenPointOne = Converter.DecimalToWords(11.1M);
+            string twelvePointTwo = Converter.DecimalToWords(12.2M);
+            string thirteenPointThree = Converter.DecimalToWords(13.3M);
+            string fourteenPointFour = Converter.DecimalToWords(14.4M);
+            string fifteenPointFive = Converter.DecimalToWords(15.5M);
+            string sixteenPointSix = Converter.DecimalToWords(16.6M);
+            string seventeenPointSeven = Converter.DecimalToWords(17.7M);
+            string eighteenPointEight = Converter.DecimalToWords(18.8M);
+            string thirty = Converter.DecimalToWords(30M);
+            string forty = Converter.DecimalToWords(40m);
+            string nineteenPointNine = Converter.DecimalToWords(19.9M);
+            string twentyOnePointOne = Converter.DecimalToWords(21.1M);
+            string thirtyOnePointOne = Converter.DecimalToWords(31.1M);
+            string fortyOnePointOne = Converter.DecimalToWords(41.1M);
+            string fiftyOnePointOne = Converter.DecimalToWords(51.1M);
+            string sixtyOnePointOne = Converter.DecimalToWords(61.1M);
+            string seventyOnePointOne = Converter.DecimalToWords(71.1M);
+            string eightyOnePointOne = Converter.DecimalToWords(81.1M);
+            string ninetyOnePointOne = Converter.DecimalToWords(91.1M);
+            string oneHundred = Converter.DecimalToWords(100M);
+
+            
+        }
+    }
+}
